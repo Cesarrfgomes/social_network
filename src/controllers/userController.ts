de66a6edce9bdd55898a51767d1395a2f01ae91e
César Gomes <filipegomes0127642@gmail.com>
2023-12-09 16:42:46 -0300

    feat: criacao das rotas de cadastro, listagem e atualizacao de usuarios

---
+++ src/controllers/userController.ts
@@ -0,0 +1,94 @@
+import {Request, Response} from 'express'
+import bcrypt from 'bcrypt'
+import User from '../models/User'
+
+export class UserController{
+    async get(req: Request, res: Response){
+        try {
+            const users = await User.find()
+
+            return res.status(200).json(users)
+        } catch (error) {
+            return res.status(500).json({message: "Erro interno do servidor."})
+        }
+    }
+
+    async show(req: Request, res: Response){
+        const {id} = req.params
+
+        try {
+            const user = await User.findById(id)
+
+            return res.status(200).json(user)
+        } catch (error) {
+            return res.status(500).json({message: "Erro interno do servidor."})
+        }
+    }
+
+    async create(req: Request, res: Response){
+        const {name, email, username, password, photo, description} =req.body
+
+        try {
+            const existUserEmailOrUsername = await User.findOne({
+                $or: [{email}, {username}]
+            })
+
+            if(existUserEmailOrUsername){
+                return res.status(400).json({message: "Username ou email já está em uso."})
+            }
+
+            const encryptedPassword = await bcrypt.hash(password, 10)
+
+            const newUser = await User.create({
+                name,
+                email,
+                username,
+                password: encryptedPassword,
+                photo,
+                description,
+                isActive: true,
+                isVerified: false,
+                createdAt: new Date
+            })
+
+            if(!newUser){
+                return res.status(400).json({message: "Nao foi possivel cadastrar o usuário."})
+            }
+            
+            return res.status(201).json(newUser)
+        } catch (error) {
+            return res.status(500).json({message: "Erro interno do servidor."})
+        }
+    }
+
+    async update(req: Request, res: Response){
+        const {name, email, username, password, photo, description} =req.body
+        const {id} = req.params
+        try {
+            const existUserEmailOrUsername = await User.findOne({
+                $or: [{email, _id : {$ne: id}}, {username, _id : {$ne: id}}]
+            })
+
+            if(existUserEmailOrUsername){
+                return res.status(400).json({message: "Username ou email já está em uso."})
+            }
+
+            const encryptedPassword = await bcrypt.hash(password, 10)
+
+             await User.updateOne({_id: id},{
+                name,
+                email,
+                username,
+                password: encryptedPassword,
+                photo,
+                description
+            })
+
+
+            
+            return res.status(204).json()
+        } catch (error) {
+            return res.status(500).json({message: "Erro interno do servidor."})
+        }
+}
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express'
+import { UserController } from './controllers/userController'
 
 const routes= Router()
 
-routes.get('/', (req, res) =>{
-    return res.json("Tudo certo")
-})
+routes.post('/user', new UserController().create)
+routes.get('/user', new UserController().get)
+routes.get('/user/:id', new UserController().show)
 
 export default routes
