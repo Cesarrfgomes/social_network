2ba335b5b6fbdf4c92c5ea66a8ef4ea087cee140
unknown <filipegomes012642@gmail.com>
2023-12-15 19:36:55 -0300

    feat: endpoint para deletar postagem criado

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -69,4 +69,21 @@ export class PostController {
             return res.status(500).json({ message: "Erro interno do servidor." })
         }
     }
+    async excluir(req: Request, res: Response) {
+        const { id } = req.params
+        try {
+            const post = await Post.findById(id)
+
+            if (!post) {
+                return res.status(404).json({ message: "Postagem não encontrada." })
+            }
+
+            await Post.deleteOne({ _id: id })
+
+            return res.status(204).json()
+        } catch (error) {
+            return res.status(500).json({ message: "Erro interno do servidor." })
+        }
+    }
+
 }
