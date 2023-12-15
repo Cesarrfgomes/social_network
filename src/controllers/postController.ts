8c0144da4a1962256cd616a568d0b7276189660b
unknown <filipegomes012642@gmail.com>
2023-12-15 19:27:46 -0300

    feat: endpoint de edicao de postagem criado

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -49,4 +49,24 @@ export class PostController {
             return res.status(500).json({ message: "Erro interno do servidor." })
         }
     }
+
+    async update(req: Request, res: Response) {
+        const { description } = req.body
+        const { id } = req.params
+        try {
+            const post = await Post.findById(id)
+
+            if (!post) {
+                return res.status(404).json({ message: "Postagem não encontrada." })
+            }
+
+            await Post.updateOne({ _id: id }, {
+                description
+            })
+
+            return res.status(204).json()
+        } catch (error) {
+            return res.status(500).json({ message: "Erro interno do servidor." })
+        }
+    }
 }
